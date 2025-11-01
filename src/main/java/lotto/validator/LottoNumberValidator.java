625c33d078ed2f9fa83efdea4eb0fd9e145d903d
dlsnfl0615 <hyunseo0750@gmail.com>
2025-11-01 21:44:34 +0900

    feat(LottoNumberValidator): 생성된 숫자가 1부터 45 사이인지 검증

---
+++ src/main/java/lotto/validator/LottoNumberValidator.java
@@ -0,0 +1,13 @@
+package lotto.validator;
+
+public class LottoNumberValidator {
+    private static final int minimumNumber = 1;
+    private static final int maximumNumber = 45;
+    private static final String errorMessage = "[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.";
+
+    public static void isInRange(int generatedNumber) {
+        if (generatedNumber < minimumNumber || generatedNumber > maximumNumber) {
+            throw new IllegalArgumentException(errorMessage);
+        }
+    }
+}
