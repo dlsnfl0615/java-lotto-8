eb6d83c762e59f09879055c3b631fbc6223b24f7
dlsnfl0615 <hyunseo0750@gmail.com>
2025-11-01 22:17:05 +0900

    feat(LottoGenerator): 1~45 범위의 숫자 6개를 생성해 List로 반환

---
+++ src/main/java/lotto/domain/LottoGenerator.java
@@ -0,0 +1,10 @@
+package lotto.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
+
+public class LottoGenerator {
+    public static List<Integer> generateNumbers() {
+        return Randoms.pickUniqueNumbersInRange(1, 45, 6);
+    }
+}
