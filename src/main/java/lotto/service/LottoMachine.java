57f7a1268c231fc9f2d1b5a4795e975f47a0907c
dlsnfl0615 <hyunseo0750@gmail.com>
2025-11-03 07:06:55 +0900

    refactor(LottoMachine): service 계층으로 이동

---
+++ src/main/java/lotto/service/LottoMachine.java
@@ -0,0 +1,40 @@
+package lotto.service;
+
+import lotto.domain.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoMachine {
+    private final WinningStatistics statistics;
+
+    public LottoMachine(WinningStatistics statistics) {
+        this.statistics = statistics;
+    }
+
+    public List<List<Integer>> buyLotto(int buyAmount) {
+        List<List<Integer>> lottoTickets = new ArrayList<>();
+
+        for (int i = 0; i < buyAmount / 1000; i++) {
+            List<Integer> ticket = LottoGenerator.generateNumbers();
+            lottoTickets.add(ticket);
+        }
+
+        return lottoTickets;
+    }
+
+    public List<String> calculateRanks(List<List<Integer>> lottoTickets, List<Integer> winningNumbers, int bonusNumber, int buyAmount) {
+        for (List<Integer> ticketNumbers : lottoTickets) {
+            Lotto lottoTicket = new Lotto(ticketNumbers);
+
+            int regularMatchCount = lottoTicket.countRegularNumbers(winningNumbers);
+            int bonusMatchCount = lottoTicket.countBonusNumber(bonusNumber);
+
+            Rank rank = RankCalculator.calculateRank(regularMatchCount, bonusMatchCount);
+
+            statistics.addRank(rank);
+        }
+
+        return statistics.getStatistics(buyAmount);
+    }
+}
