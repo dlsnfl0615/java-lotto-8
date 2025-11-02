e4b4f7c3c3d9ec8d5182d98dfd8511cf0c32834f
dlsnfl0615 <hyunseo0750@gmail.com>
2025-11-02 17:01:13 +0900

    feat(WinningStatistics): 로또 당첨 통계 계산
    - 각 등수에 몇 개 당첨됐는지, 수익률은 얼만지 저장한 리스트 반환

---
+++ src/main/java/lotto/domain/WinningStatistics.java
@@ -0,0 +1,51 @@
+package lotto.domain;
+
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class WinningStatistics {
+    private final Map<Rank, Integer> winningCount = new HashMap<>();
+    private final int buyAmount;
+
+    public WinningStatistics(int buyAmount) {
+        winningCount.put(Rank.FIRST, 0);
+        winningCount.put(Rank.SECOND, 0);
+        winningCount.put(Rank.THIRD, 0);
+        winningCount.put(Rank.FOURTH, 0);
+        winningCount.put(Rank.FIFTH, 0);
+        winningCount.put(Rank.FAIL, 0);
+        this.buyAmount = buyAmount;
+    }
+
+    public void addRank(Rank rank) {
+        winningCount.put(rank, winningCount.get(rank) + 1);
+    }
+
+    public List<String> getStatistics() {
+        List<String> result = new ArrayList<>();
+
+        result.add(winningCount.get(Rank.FIFTH).toString());
+        result.add(winningCount.get(Rank.FOURTH).toString());
+        result.add(winningCount.get(Rank.THIRD).toString());
+        result.add(winningCount.get(Rank.SECOND).toString());
+        result.add(winningCount.get(Rank.FIRST).toString());
+
+        result.add(getReturnRate());
+
+        return result;
+    }
+
+    public String getReturnRate() {
+        double sum = 0;
+        Set<Rank> keySet = winningCount.keySet();
+        for (Rank rank : keySet) {
+            sum += winningCount.get(rank) * rank.getMoney();
+        }
+
+        return String.format("%.1f", sum / buyAmount);
+    }
+}
