23f7e713950c093ef375628cac1352600096730e
dlsnfl0615 <hyunseo0750@gmail.com>
2025-11-03 06:56:58 +0900

    feat(Application): 컨트롤러 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,21 @@
 package lotto;
 
+import lotto.controller.LottoController;
+import lotto.service.LottoMachine;
+import lotto.domain.WinningStatistics;
+import lotto.validator.InputNumberValidator;
+import lotto.view.InputNumbers;
+import lotto.view.OutputStatistics;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputNumbers input = new InputNumbers();
+        OutputStatistics output = new OutputStatistics();
+        InputNumberValidator validator = new InputNumberValidator();
+        LottoMachine machine = new LottoMachine(new WinningStatistics());
+
+        LottoController controller = new LottoController(input, output, validator, machine);
+
+        controller.run();
     }
 }
