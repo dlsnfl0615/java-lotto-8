f24574bcf63ee6e2bd03d21d072e2c7de0e79a4b
dlsnfl0615 <hyunseo0750@gmail.com>
2025-11-03 06:57:19 +0900

    feat(ErrorMessages): 에러 메시지 모아둔 enum 구현

---
+++ src/main/java/lotto/error/ErrorMessages.java
@@ -0,0 +1,20 @@
+package lotto.error;
+
+public enum ErrorMessages {
+    OUT_OF_RANGE("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다."),
+    NOT_COMMA("[ERROR] 각 숫자는 쉼표로만 구분되어야 합니다: "),
+    NOT_MULTIPLE_THOUSAND("[ERROR] 구매 금액은 1000의 배수이어야 합니다."),
+    NOT_DIGIT("[ERROR] 구매 금액은 숫자만 가능합니다."),
+    DUPLICATION("[ERROR] 로또 번호는 중복될 수 없습니다."),
+    NUMBER_COUNT("[ERROR] 로또 번호는 6개여야 합니다.");
+
+    private final String message;
+
+    ErrorMessages(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
