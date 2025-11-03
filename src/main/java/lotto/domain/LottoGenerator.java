af4fa60cdb771aa926f2c48d3e25cdb16cf5e7dd
dlsnfl0615 <hyunseo0750@gmail.com>
2025-11-03 20:53:08 +0900

    refactor: static 메소드 사용시 생성자를 private로 감추기

--- src/main/java/lotto/domain/LottoGenerator.java
+++ src/main/java/lotto/domain/LottoGenerator.java
@@ -4,6 +4,8 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
 
 public class LottoGenerator {
+    private LottoGenerator() {}
+
     public static List<Integer> generateNumbers() {
         return Randoms.pickUniqueNumbersInRange(1, 45, 6);
     }
