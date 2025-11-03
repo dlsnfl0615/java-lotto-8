03e3479897f23c07de87debd5d9be6c457ae90f0
dlsnfl0615 <hyunseo0750@gmail.com>
2025-11-03 15:24:21 +0900

    feat(ZERO): 구매 금액은 0보다 커야 한다는 오류 메시지 추가

--- src/main/java/lotto/error/ErrorMessages.java
+++ src/main/java/lotto/error/ErrorMessages.java
@@ -6,7 +6,8 @@ public enum ErrorMessages {
     NOT_MULTIPLE_THOUSAND("[ERROR] 구매 금액은 1000의 배수이어야 합니다."),
     NOT_DIGIT("[ERROR] 구매 금액은 숫자만 가능합니다."),
     DUPLICATION("[ERROR] 로또 번호는 중복될 수 없습니다."),
-    NUMBER_COUNT("[ERROR] 로또 번호는 6개여야 합니다.");
+    NUMBER_COUNT("[ERROR] 로또 번호는 6개여야 합니다."),
+    ZERO("[ERROR] 최소 한 개 이상의 로또를 구매해야 합니다.");
 
     private final String message;
 
