63b68203f36073599e2847ee80a64afb983f52d1
dlsnfl0615 <hyunseo0750@gmail.com>
2025-11-03 15:27:37 +0900

    test: 사용자 입력에 대한 단위 테스트 작성

--- src/test/java/lotto/ApplicationTest.java
+++ src/test/java/lotto/ApplicationTest.java
@@ -54,6 +54,60 @@ class ApplicationTest extends NsTest {
         });
     }
 
+    // 추가 테스트 코드 작성
+
+    @Test
+    void 모든_등수_테스트() {
+        assertRandomUniqueNumbersInRangeTest(
+                () -> {
+                    run("5000", "1,2,3,4,5,6", "7");
+                    assertThat(output()).contains(
+                            "5개를 구매했습니다.",
+                            "[1, 2, 3, 43, 44, 45]",
+                            "[1, 2, 3, 4, 44, 45]",
+                            "[1, 2, 3, 4, 5, 45]",
+                            "[1, 2, 3, 4, 5, 7]",
+                            "[1, 2, 3, 4, 5, 6]",
+                            "3개 일치 (5,000원) - 1개",
+                            "4개 일치 (50,000원) - 1개",
+                            "5개 일치 (1,500,000원) - 1개",
+                            "5개 일치, 보너스 볼 일치 (30,000,000원) - 1개",
+                            "6개 일치 (2,000,000,000원) - 1개",
+                            "총 수익률은 40631100.0%입니다."
+                    );
+                },
+                List.of(1,2,3,43,44,45),
+                List.of(1,2,3,4,44,45),
+                List.of(1,2,3,4,5,45),
+                List.of(1,2,3,4,5,7),
+                List.of(1,2,3,4,5,6)
+        );
+    }
+
+    @Test
+    void 구매_금액_콤마는_인정() {
+        assertSimpleTest(() -> {
+            runException("1,000");
+            assertThat(output()).doesNotContain(ERROR_MESSAGE);
+        });
+    }
+
+    @Test
+    void 구매_금액_1000단위_아님() {
+        assertSimpleTest(() -> {
+            runException("3180");
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
+    }
+
+    @Test
+    void 구매_금액이_0() {
+        assertSimpleTest(() -> {
+            runException("0");
+            assertThat(output()).contains(ERROR_MESSAGE);
+        });
+    }
+
     @Override
     public void runMain() {
         Application.main(new String[]{});
