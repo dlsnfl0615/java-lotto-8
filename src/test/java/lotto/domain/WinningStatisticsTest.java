7ef7ab2e21f0188055715c4862fae5d54c62ecf9
dlsnfl0615 <hyunseo0750@gmail.com>
2025-11-03 15:28:25 +0900

    test: 각 등수의 결과에 대한 단위 테스트 작성

--- src/test/java/lotto/domain/WinningStatisticsTest.java
+++ src/test/java/lotto/domain/WinningStatisticsTest.java
@@ -1,18 +1,32 @@
 package lotto.domain;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class WinningStatisticsTest {
-    @Test
-    void 로또_3등_당첨() {
+    @ParameterizedTest()
+    @MethodSource("ranks")
+    void 모든_등수_하나씩_당첨(List<String> output, Rank rank) {
         WinningStatistics statistics = new WinningStatistics();
-        statistics.addRank(Rank.THIRD);
-        List<String> result = statistics.getStatistics(5000);
+        statistics.addRank(rank);
+        List<String> result = statistics.getStatistics(1000);
 
-        assertThat(result).containsExactly("0", "0", "1", "0", "0", "30000.0");
+        assertThat(result).isEqualTo(output);
+    }
+
+    static Stream<Arguments> ranks() {
+        return Stream.of(
+                Arguments.of(List.of("1", "0", "0", "0", "0", "500.0"), Rank.FIFTH),
+                Arguments.of(List.of("0", "1", "0", "0", "0", "5000.0"), Rank.FOURTH),
+                Arguments.of(List.of("0", "0", "1", "0", "0", "150000.0"), Rank.THIRD),
+                Arguments.of(List.of("0", "0", "0", "1", "0", "3000000.0"), Rank.SECOND),
+                Arguments.of((List.of("0", "0", "0", "0", "1", "200000000.0")), Rank.FIRST)
+        );
     }
 }
